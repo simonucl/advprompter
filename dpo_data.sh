9dd6a67dd304ded2e544faa1729d6d2163731c33
simon <simon011130@gmail.com>
2024-08-24 09:39:03 +0000

    add new gen data

--- dpo_data.sh
+++ dpo_data.sh
@@ -5,9 +5,23 @@ MODELS=(
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 BASE_DIR=output/prompter/aya_opt
 
+# for MODEL in "${MODELS[@]}"
+# do
+#     DATA_DIR=$BASE_DIR/suffix_dataset
+#     MODEL_BASE_NAME=$(basename $MODEL)
+#     OUTPUT_DIR=$DATA_DIR/${MODEL_BASE_NAME}_advbench
+#     mkdir -p $OUTPUT_DIR
+# 
+#     python3 eval_jailbreak.py \
+#         --model $MODEL \
+#         --data_dir $DATA_DIR \
+#         --output_dir $OUTPUT_DIR \
+# 	--parallel 32
+# done
+
 for MODEL in "${MODELS[@]}"
 do
-    DATA_DIR=$BASE_DIR/suffix_dataset
+    DATA_DIR=data/harmful_behaviors/dataset
     MODEL_BASE_NAME=$(basename $MODEL)
     OUTPUT_DIR=$DATA_DIR/${MODEL_BASE_NAME}_advbench
     mkdir -p $OUTPUT_DIR
@@ -15,18 +29,6 @@ do
     python3 eval_jailbreak.py \
         --model $MODEL \
         --data_dir $DATA_DIR \
-        --output_dir $OUTPUT_DIR
-done
-
-for MODEL in "${MODELS[@]}"
-do
-    DATA_DIR=data/harmful_behaviors
-    MODEL_BASE_NAME=$(basename $MODEL)
-    OUTPUT_DIR=$DATA_DIR/${MODEL_BASE_NAME}_advprompter
-    mkdir -p $OUTPUT_DIR
-
-    python3 eval_jailbreak.py \
-        --model $MODEL \
-        --data_dir $DATA_DIR \
-        --output_dir $OUTPUT_DIR
+        --output_dir $OUTPUT_DIR \
+	--parallel 32
 done

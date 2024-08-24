7d4cf78dc598b29f599b133e6854bd99b6ffa3b4
simonucl <simon011130@gmail.com>
2024-08-24 13:51:55 +0800

    add new conf

--- generate.sh
+++ generate.sh
@@ -1,2 +1,4 @@
-mkdir -p output/prompter/aya_opt
-python3 gen_advprompt.py --model_param_path conf/prompter/aya_opt.yaml --output_dir output/prompter/aya_opt
+OUTPUT_DIR=output/prompter/llama3_opt
+mkdir -p $OUTPUT_DIR
+python3 gen_advprompt.py \
+    --model_param_path conf/prompter/llama3_opt.yaml --output_dir $OUTPUT_DIR

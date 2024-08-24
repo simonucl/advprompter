b34341b0fbbb3b552545d1661bf917f1bce88cb4
simonucl <simon011130@gmail.com>
2024-08-24 12:26:23 +0800

    add jailbreak script

--- jailbreak.sh
+++ jailbreak.sh
@@ -1,43 +1,21 @@
-MODEL=CohereForAI/aya-23-8B
-DATA_DIR=output/prompter/aya_opt/suffix_dataset
-MODEL_BASE_NAME=$(basename $MODEL)
-OUTPUT_DIR=output/prompter/aya_opt/$MODEL_BASE_NAME
-mkdir -p $OUTPUT_DIR
+MODELS=(
+    "CohereForAI/aya-23-8B"
+    "CohereForAI/aya-23-35B"
+    "google/gemma-2-9b-it"
+    "meta-llama/Meta-Llama-3-8B-Instruct"
+)
 
-python3 eval_jailbreak.py \
-    --model $MODEL \
-    --data_dir $DATA_DIR \
-    --output_dir $OUTPUT_DIR
+MODELS=("CohereForAI/aya-23-35B")
 
-MODEL=CohereForAI/aya-23-35B
-DATA_DIR=output/prompter/aya_opt/suffix_dataset
-MODEL_BASE_NAME=$(basename $MODEL)
-OUTPUT_DIR=output/prompter/aya_opt/$MODEL_BASE_NAME
-mkdir -p $OUTPUT_DIR
+for MODEL in "${MODELS[@]}"
+do
+    DATA_DIR=output/prompter/aya_opt/suffix_dataset
+    MODEL_BASE_NAME=$(basename $MODEL)
+    OUTPUT_DIR=output/prompter/aya_opt/$MODEL_BASE_NAME
+    mkdir -p $OUTPUT_DIR
 
-python3 eval_jailbreak.py \
-    --model $MODEL \
-    --data_dir $DATA_DIR \
-    --output_dir $OUTPUT_DIR
-
-MODEL=google/gemma-2-9b-it
-DATA_DIR=output/prompter/aya_opt/suffix_dataset
-MODEL_BASE_NAME=$(basename $MODEL)
-OUTPUT_DIR=output/prompter/aya_opt/$MODEL_BASE_NAME
-mkdir -p $OUTPUT_DIR
-
-python3 eval_jailbreak.py \
-    --model $MODEL \
-    --data_dir $DATA_DIR \
-    --output_dir $OUTPUT_DIR
-
-MODEL=meta-llama/Meta-Llama-3-8B-Instruct
-DATA_DIR=output/prompter/aya_opt/suffix_dataset
-MODEL_BASE_NAME=$(basename $MODEL)
-OUTPUT_DIR=output/prompter/aya_opt/$MODEL_BASE_NAME
-mkdir -p $OUTPUT_DIR
-
-python3 eval_jailbreak.py \
-    --model $MODEL \
-    --data_dir $DATA_DIR \
-    --output_dir $OUTPUT_DIR
+    python3 eval_jailbreak.py \
+        --model $MODEL \
+        --data_dir $DATA_DIR \
+        --output_dir $OUTPUT_DIR
+done
